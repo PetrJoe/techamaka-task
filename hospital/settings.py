4fe60b8ae539a4ff95393a89b52e5396328bd68c
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2024-02-05 20:56:24 +0100

    Update settings.py

--- hospital/settings.py
+++ hospital/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
